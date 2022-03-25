1e468acf70966b2d9a0523fe29b79c1a6094754a
Maia Viera Cañive <76230421+mvieracanive@users.noreply.github.com>
2022-03-25 15:21:48 -0400

    Add files via upload

--- docker-install.sh
+++ docker-install.sh
@@ -1,11 +1,11 @@
  #!/bin/bash
 
-curl https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/containerd.io_1.5.11-1_amd64.deb
+curl -o containerd.io.deb https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/containerd.io_1.5.11-1_amd64.deb
 
-curl https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce-cli_20.10.9~3-0~ubuntu-bionic_amd64.deb
+curl -o docker-ce-cli.deb https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce-cli_20.10.9~3-0~ubuntu-bionic_amd64.deb
 
-curl https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce_20.10.9~3-0~ubuntu-bionic_amd64.deb
+curl -o docker-ce.deb https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce_20.10.9~3-0~ubuntu-bionic_amd64.deb
 
-sudo dpkg -i docker-ce_20.10.9~3-0~ubuntu-bionic_amd64.deb
-sudo dpkg -i docker-ce-cli_20.10.9~3-0~ubuntu-bionic_amd64.deb
-sudo dpkg -i containerd.io_1.5.11-1_amd64.deb
+sudo dpkg -i docker-ce.deb
+sudo dpkg -i docker-ce-cli.deb
+sudo dpkg -i containerd.io.deb
