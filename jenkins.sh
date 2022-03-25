77e698c6c26adc60cb2b0ac1dde7243381d16397
Maia Viera Cañive <76230421+mvieracanive@users.noreply.github.com>
2022-03-25 18:15:01 -0400

    Add files via upload

--- jenkins.sh
+++ jenkins.sh
@@ -1,1 +1,7 @@
-curl -o jenkins https://www.jenkins.io/download
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+apt-get update
+apt-get install fontconfig openjdk-11-jre
+apt-get install jenkins
